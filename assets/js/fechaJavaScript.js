b066866844ebd167a8b445aae111461177c512e8
joeldelgado505 <joefernandez505@gmail.com>
2023-01-19 11:11:31 +0100

    modified:   assets/js/fechaJavaScript.js

--- assets/js/fechaJavaScript.js
+++ assets/js/fechaJavaScript.js
@@ -1,3 +1,3 @@
     const d = new Date();
-	var piepagina = "<p>Made with <i class='fa fa-heart'></i> by <a href='http://bootstrapthemes.co'>Bootstrap Themes</a>" + d.getFullYear()+" All Rights Reserved</p>"
+	var piepagina = "<p class='derechosReservados'>Made with <i class='fa fa-heart'></i> by <a href='http://bootstrapthemes.co'>Bootstrap Themes</a>" + d.getFullYear()+" All Rights Reserved</p>"
 	document.getElementById("piepagina").innerHTML = piepagina ;
