06ffa52fcb05d6cb87f57af4bafd35095f7bcdc4
joeDelgado505 <joefernandez505@gmail.com>
2023-02-17 10:43:54 +0100

    modified:   assets/css/myStyle.css 	new file:   assets/images/pizza_pala_pdf_pages-to-jpg-0001.jpg 	new file:   assets/images/pizza_pala_pdf_pages-to-jpg-0002.jpg 	new file:   assets/images/pizza_pala_pdf_pages-to-jpg-0002.png 	new file:   assets/images/prueba.html 	new file:   assets/images/scooter.png 	new file:   assets/images/scooter.svg 	modified:   assets/js/myjs.js 	modified:   assets/php/EnviarPizza.php 	modified:   index.html

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -2,7 +2,7 @@
 var url = location.href;
 const d = new Date();
 
-var piepagina = "Politicas y cookies<p class='derechosReservados'>Todos los derechos reservados a Georgy Fieraru y Joel Delgado<i class='fa fa-heart'></i> by <a href='http://bootstrapthemes.co'>Bootstrap Themes</a>" + d.getFullYear() + "</p>"
+var piepagina = "Políticas y cookies<p class='derechosReservados'>Todos los derechos reservados a Georgy Fieraru y Joel Delgado<i class='fa fa-heart'></i> by <a href='http://bootstrapthemes.co'>Bootstrap Themes</a>" + d.getFullYear() + "</p>"
 var webCookies = url.indexOf("Cookies.html");
 var webPoliticas = url.indexOf("politicas.html");
 
@@ -23,7 +23,7 @@ var webPoliticas = url.indexOf("politicas.html");
 ///////////////////////////Pie de pagina//////////////////
 
 
-var piepagina = "Politicas y cookies<p class='derechosReservados'>Todos los derechos reservados a Georgy Fieraru y Joel Delgado<i class='fa fa-heart'></i> by <a href='http://bootstrapthemes.co'>Bootstrap Themes</a>" + d.getFullYear() + "</p>"
+var piepagina = "Políticas y cookies<p class='derechosReservados'>Todos los derechos reservados a Georgy Fieraru y Joel Delgado<i class='fa fa-heart'></i> by <a href='http://bootstrapthemes.co'>Bootstrap Themes</a>" + d.getFullYear() + "</p>"
 
 if (webPoliticas < 0 || webCookies<0) {
 	piepagina = "<p class='derechosReservados'>"+d.getFullYear()+ " Copyright PizzaPala. Diseñado y Desarrollado por Georgy Fieraru y Joel Delgado<i class='fa fa-heart'></i> by <a href='http://bootstrapthemes.co'>Bootstrap Themes</a></p>";
@@ -39,7 +39,7 @@ var parteAbajo ="<div class='col-md-4'>"
 							+"<p>"
 								+"<div class='date_day'>JUEVES A DOMINGO</div>"
 								+"<div>Desde 19:30 Hasta 00:00</div>"
-								+"<div class='phone_email'>Tel: 605 19 65 01</div>"
+								+"<div class='phone_email'>Tel: <a href='tel:605196501' class='enlaceFooter'>605 19 65 01</a></div>"
 								+"<div>Entregas a domicilio</div>"
 								+"<div>Comida para llevar</div>"
 								+"<div class='footer_socail_icon Insta'>"
@@ -48,8 +48,8 @@ var parteAbajo ="<div class='col-md-4'>"
 										+"</a>"
 									+"</div>"
 									+"<div>"
-										+"<a href='politicas.html' style='color:yellow;'>Politica de privacidad</a>"
-										+"<a style='color:yellow;display:block;' href='Cookies.html'>Politica de cookies</a>"
+										+"<a href='politicas.html' class='enlaceFooter'>Política de privacidad</a>"
+										+"<a style='display:block;'class='enlaceFooter' href='Cookies.html'>Política de cookies</a>"
 									+"</div>"
 								+"</div>"
 							+"</p>"
@@ -61,7 +61,7 @@ var parteAbajo ="<div class='col-md-4'>"
 						+"<h3 class='letrafooter'>Pizza Pala se encuentra</h3>"
 						+"<div class='single_widget_info'>"
 							+"<div>"
-								+"<a href='https://www.google.com/maps?ll=39.188611,-0.436522&z=18&t=m&hl=es&gl=ES&mapclient=embed&cid=8801043269333629544' style='color:yellow;'>Plaça Carbó N1 - Algemesí - Valencia</a>"
+								+"<a href='https://www.google.com/maps?ll=39.188611,-0.436522&z=18&t=m&hl=es&gl=ES&mapclient=embed&cid=8801043269333629544' class='enlaceFooter'>Plaça Carbó N1 - Algemesí - Valencia</a>"
 							+"</div>"
 							+"<iframe src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d546.6621582067178!2d-0.43684885696660664!3d39.188628118888246!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd61b1cab763a113%3A0x7a23963939793a68!2sPIZZA%20PALA!5e0!3m2!1ses!2ses!4v1675338867927!5m2!1ses!2ses' width='325' height='270' style='border:0;' allowfullscreen='' loading='lazy' referrerpolicy='no-referrer-when-downgrade'>"+"</iframe>"
 						+"</div>"
@@ -73,10 +73,10 @@ var parteAbajo ="<div class='col-md-4'>"
 						+"<div class='single_widget_form text-left'>"
 							+"<form id='formid' method='post' >"
 								+"<div class='form-group'>"
-									+"<input id='Nombre' type='text' class='form-control' name='nombre' placeholder='Nombres'>"
+									+"<input id='Nombre' type='text' class='form-control' name='nombre' placeholder='Nombre'>"
 								+"</div>"
 								+"<div class='form-group'>"
-									+"<input id='Tel' type='text' class='form-control' name='tel' placeholder='Telefono'>"
+									+"<input id='Tel' type='text' class='form-control' name='tel' placeholder='Teléfono'>"
 								+"</div>"
 								+"<div class='form-group'>"
 									+"<input id='Email' type='email' class='form-control' name='email' placeholder='Email'>"
@@ -84,7 +84,7 @@ var parteAbajo ="<div class='col-md-4'>"
 								+"<div class='form-group' id='mensaje'>"
 									+"<textarea id='Comentario' class='form-control' name='comentario' rows='3' placeholder='Mensaje'>"+"</textarea>"
 								+"</div>"
-								+"<input type='checkbox' id='checkedPoliticas' value='Acepto' required> Acepto la <a href ='politicas.html' style='color:yellow;' >politica de privacidad</a>"+"</input>"
+								+"<input type='checkbox' id='checkedPoliticas' value='Acepto' required> Acepto la <a href ='politicas.html' class='enlaceFooter' >política de privacidad</a>"+"</input>"
 
 								+"<div class='form-group' id='enviar'>"
 									+"<input id='botonEnviar' type='submit' value='Enviar' class='btn btn-primary fuenteChin_boton'>"+"</input>"
